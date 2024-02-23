5fb2271b231aa1440e36b639c8c0cff6e18d4c05
RaccoonCodes <61885457+RaccoonCodes@users.noreply.github.com>
2024-02-22 19:06:25 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            var products = new[] {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+            };
+            return View(products.Select(p =>
+                $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
+        }
+
+        // public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
+        //public ViewResult Index()
+        //{
+        //    return View(Product.GetProducts().Select(p => p?.Name));
+        //}
+
+    }
+}
